5a02dcd62e5dfea0fdf5e0f29fb91113748dbf30
jakubkarczewski <jakub.jan.karczewski@intel.com>
2017-10-26 01:26:06 +0200

    Dataset split utility and daily sentiment score

--- infer.py
+++ infer.py
@@ -1,5 +1,5 @@
 """ Entry point for model infering. """
-
+import pandas as pd
 
 from model.classifier import Classifier
 from data_utils.preprocessing import encode_sentences, tokenize_sentences
@@ -14,13 +14,24 @@ def main():
     result = classifier.infer(encoded_sentences)
     print(result)
 
+
 def classify(tweet_body):
+    classifier = Classifier("data/best_model/model.meta",
+                            "data/best_model/model")
     sentences = [["0", tweet_body]]
     tokenized_sentences = tokenize_sentences(sentences)
     encoded_sentences, _ = encode_sentences(tokenized_sentences, 32)
     result = classifier.infer(encoded_sentences)
     return result
-    
+
+
+def _get_sentiment_vector(date, tweets_dir):
+    all_data = pd.read_csv('%s/%s.csv' % (tweets_dir, date))
+    tweets_body = all_data['Tweet content']
+    sentiment_vector = [classify(row) for row in tweets_body[1:]]
+    return sentiment_vector
+
     
 if __name__ == "__main__":
     main()
+    # print(_get_sentiment_vector('2016-05-03', 'nasdaq_tweets'))
---
+++ split_nasdaq_tweets.py
@@ -0,0 +1,23 @@
+import subprocess
+
+import pandas as pd
+
+
+def split_datasets(file_path, sheet_number, dir_name):
+
+    subprocess.call(['mkdir', dir_name])
+    all_sheets = pd.ExcelFile(file_path)
+    one_sheet = all_sheets.parse(sheet_number)
+
+    date_body_lang = one_sheet.filter(['Date', 'Tweet content',
+                                       'Tweet language (ISO 639-1)'])
+    date_body_lang = date_body_lang.groupby('Date')
+
+    all_days = [date_body_lang.get_group(x) for x in date_body_lang.groups]
+    for one_day in all_days:
+        one_day.to_csv('./nasdaq_tweets/%s.csv' % one_day['Date'].iloc[2])
+
+
+if __name__ == '__main__':
+    split_datasets('corp_tweets/export_dashboard_aapl_2016_06_15_14_30_09.xlsx',
+                   1, 'nasdaq_tweets')
