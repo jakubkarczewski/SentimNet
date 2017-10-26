e01358401a51fa0c1455090803a2476194b3610f
jakubkarczewski <jakub.jan.karczewski@intel.com>
2017-10-26 15:33:33 +0200

    Fixed bug

--- infer.py
+++ infer.py
@@ -29,24 +29,25 @@ def classify(tweet_body):
 
 
 def _get_sentiment_vector(date, tweets_dir):
-    all_data = pd.read_csv('%s/%s.csv' % (tweets_dir, date))
+    all_data = pd.read_csv('./%s/%s.csv' % (tweets_dir, date))
     tweets_body = all_data['Tweet content']
     sentiment_vector = [classify(row) for row in tweets_body[1:]]
     return sentiment_vector
 
 
 def write_sentiment_csv(tweets_dir):
-    all_files = os.listdir('/%s' % tweets_dir)
+    all_files = os.listdir('./%s' % tweets_dir)
     for file in all_files:
         sentiment_vector = _get_sentiment_vector(file[:-4], tweets_dir)
-        sentiment_score_f = open('%s/%s_score.csv' % (tweets_dir, file[:-4]))
+        sentiment_score_f = open('./%s/%s_score.csv' % (tweets_dir, file[:-4]),
+                                 'w')
         for elem in sentiment_vector:
             sentiment_score_f.write('%s\n' % elem)
 
 
 if __name__ == "__main__":
-    main()
+    #main()
     split_nasdaq_tweets.split_datasets(
-        'corp_tweets/export_dashboard_aapl_2016_06_15_14_30_09.xlsx',
-        1, 'nasdaq_tweets')
+       'corp_tweets/export_dashboard_aapl_2016_06_15_14_30_09.xlsx',
+       1, 'nasdaq_tweets')
     write_sentiment_csv('nasdaq_tweets')
